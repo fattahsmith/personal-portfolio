f68687f03783d0e7c7d912b92bbea74b77ed4414
smith_ <fattahsmith08@gmail.com>
2025-10-30 12:05:00 +0700

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  outputFileTracingRoot: process.cwd(),
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
+
 
 export default nextConfig;
