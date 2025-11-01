f34ccfe6e256ec54138148191ea360d42e0cb0f6
fattahsmith <fattahsmith08@gmail.com>
2025-11-01 09:37:34 +0700

    build: production ready

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  /* config options here */
+  outputFileTracingRoot: process.cwd(),
+  output: 'export',
+};
+
+module.exports = nextConfig;
